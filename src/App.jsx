079267062b5fa6ae1bfde7ecf96bc0c5c2feaef7
jamil9080 <md3711451@gmail.com>
2024-10-28 15:11:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import DaisyNav from "./component/daisynav/DaisyNav"
+
+
+function App() {
+  
+
+  return (
+    <>
+     <DaisyNav></DaisyNav>
+     
+      
+    </>
+  )
+}
+
+export default App
